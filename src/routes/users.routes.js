01ac576d1b0ce982f7eaa5cbf8aaa255ced83ad4
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-08-14 07:02:58 -0500

    feat: :sparkles: Delete and update user working

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,16 +5,16 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { log } from "console";
 
 const router = Router();
-
+//Get all users
 router.get("/", asyncHandler(async (req, res, next) => {
     const users = await getDB().collection("usuarios").find().toArray()
     res.json(users)
 }));
 
+//Get one user by id
 router.get('/:id', asyncHandler(async (req, res) => {
-
     const userId = parseInt(req.params.id, 10);
-
+    
     if (isNaN(userId)) {
         return res.status(400).json({ message: 'Invalid user id' });
     }
@@ -27,6 +27,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
     res.json(user);
 }));
+
+//Create a new user
 
 router.post("/", asyncHandler(async (req, res, next) => {
     const { nombre } = req.body;
@@ -56,12 +58,61 @@ router.post("/", asyncHandler(async (req, res, next) => {
     const result = await usuariosCollection.insertOne(newUser);
     log(result)
     res.status(201).json({
-        message: 'Usuario creado exitosamente',
+        message: "Creating user ok",
         user: newUser,
     });
 
 }))
 
+
+// Update one user by id
+router.put(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const { nombre } = req.body;
+
+    if (isNaN(userId)) {
+      return res.status(400).json({ message: 'Invalid user id' });
+    }
+
+    if (!nombre) {
+      return res.status(400).json({ message: 'Nombre es requerido' });
+    }
+
+    const result = await getDB()
+      .collection('usuarios')
+      .updateOne({ id: userId }, { $set: { nombre } });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'Updating user ok' });
+  })
+);
+
+//deleting one user by id
+router.delete(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+
+    if (isNaN(userId)) {
+      return res.status(400).json({ message: 'Invalid user id' });
+    }
+
+    const result = await getDB()
+      .collection('usuarios')
+      .deleteOne({ id: userId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'Delete user ok' });
+  })
+);
 
 
 export default router
