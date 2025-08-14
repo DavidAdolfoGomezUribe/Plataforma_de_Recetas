47ce665adda6195f38e8f33a09b1d16726c77c42
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-08-14 06:34:25 -0500

    feat: :sparkles: get user by id route done!

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,11 +3,32 @@ import { Router } from "express";
 import { getDB } from "../db/config.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-const router= Router();
+const router = Router();
 
-router.get("/",asyncHandler( async (req,res,next)=>{
+router.get("/", asyncHandler(async (req, res, next) => {
     const users = await getDB().collection("usuarios").find().toArray()
     res.json(users)
 }));
 
+router.get('/:id', asyncHandler(async (req, res) => {
+
+    const userId = parseInt(req.params.id, 10);
+
+    if (isNaN(userId)) {
+        return res.status(400).json({ message: 'Invalid user id' });
+    }
+
+    const user = await getDB().collection('usuarios').findOne({ id: userId });
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+}));
+
+
+
 export default router
+
+
