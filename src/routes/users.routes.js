edd11a4dd1c83ca244ba7752a71ca8f55967eb99
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-08-14 06:49:28 -0500

    feat: :sparkles: Create new user working

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,7 @@ import { error } from "console";
 import { Router } from "express";
 import { getDB } from "../db/config.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { log } from "console";
 
 const router = Router();
 
@@ -26,6 +27,40 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
     res.json(user);
 }));
+
+router.post("/", asyncHandler(async (req, res, next) => {
+    const { nombre } = req.body;
+
+    if (!nombre) {
+        return res.status(400).json({ message: 'Nombre es requerido' });
+    }
+
+    const db = getDB();
+    const usuariosCollection = db.collection('usuarios');
+
+
+    const lastUser = await usuariosCollection
+        .find()
+        .sort({ id: -1 })
+        .limit(1)
+        .toArray();
+
+    const newId = lastUser.length > 0 ? lastUser[0].id + 1 : 1;
+
+    const newUser = {
+        id: newId,
+        nombre,
+        recetas: [],
+    };
+
+    const result = await usuariosCollection.insertOne(newUser);
+    log(result)
+    res.status(201).json({
+        message: 'Usuario creado exitosamente',
+        user: newUser,
+    });
+
+}))
 
 
 
