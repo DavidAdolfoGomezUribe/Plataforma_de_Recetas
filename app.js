0d54da7138061b23abc5cf25cf75afb78aa738d5
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-08-14 06:22:12 -0500

    feat: :sparkles: Adding asyncHandler.js for better optimatization

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import express from "express"
 import {connect,getDB} from "./src/db/config.js"
 import {log} from "console"
-import usersRouter from "./src/routes/users.routes.js"
+import apiRouter from "./src/routes/index.js"
 log("ok")
 
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/users",usersRouter);
+app.use("/api",apiRouter);
 
 app.get("/api",(_req,res)=> res.send({status:"ok"}))
 
@@ -23,5 +23,5 @@ await connect().then(()=>{
     app.listen(PORT,()=>{
         log(`Api in http://localhost:${PORT}`)
     })
-
 })
+
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,16 +1,13 @@
 import { error } from "console";
 import { Router } from "express";
 import { getDB } from "../db/config.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
 const router= Router();
 
-router.get("/",async (req,res,next)=>{
-    try{
-        const users = await getDB().collection("usuarios").find().toArray()
-        res.json(users)
-        
-    }catch (error){
-        next(error)
-    }
-});
+router.get("/",asyncHandler( async (req,res,next)=>{
+    const users = await getDB().collection("usuarios").find().toArray()
+    res.json(users)
+}));
 
 export default router
